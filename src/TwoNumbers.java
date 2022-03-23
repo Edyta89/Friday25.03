ac63e19805757f791e6e3f712aa40fc562e2527e
Edyta89 <styrczulaedyta89@gmail.com>
2022-03-23 14:36:32 +0100

    twonumbers

---
+++ src/TwoNumbers.java
@@ -0,0 +1,19 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+        int a = 22;
+        double b = 13;
+
+        double result1 = a + b;
+        System.out.println(result1);
+         double result2 = a - b;
+        System.out.println(result2);
+        double result3= a * b;
+        System.out.println(result3);
+        double result4 = a / b;
+        System.out.println(result4);
+        int result5 = (int) (a/b);
+        System.out.println(result5);
+        int result6 = (int) ( a%b);
+        System.out.println(result6);
+    }
+}
