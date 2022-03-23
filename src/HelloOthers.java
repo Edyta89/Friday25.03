7b10743a3bcc5d7a7487a350655f344281105440
Edyta89 <styrczulaedyta89@gmail.com>
2022-03-23 14:02:17 +0100

    others

---
+++ src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Antal");
+        System.out.println("Hello Daniel");
+        System.out.println("Hello Dominik");
+    }
+}
