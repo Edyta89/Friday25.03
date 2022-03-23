bdfa314616514389c05e22e173c6f9b7e9f78aef
Edyta89 <styrczulaedyta89@gmail.com>
2022-03-23 16:33:36 +0100

    basicinfo

---
+++ src/DefineBasicInfo.java
@@ -0,0 +1,14 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Edyta";
+        int age = 33;
+        double height = 1.66;
+        boolean married = false;
+        System.out.println(name + ", " + age+" ,"+" "+ height+" ,"+ married );
+
+
+    }
+
+
+
+}
