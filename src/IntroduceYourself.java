e6bf7e074ccc8a8d6365945b2be8c97dfd1f3a06
Edyta89 <styrczulaedyta89@gmail.com>
2022-03-23 14:11:57 +0100

    introduce

---
+++ src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("My name is Edyta");
+        System.out.println("I'm " +33);
+        System.out.println("I'm "+ 166 + "cm");
+    }
+}
