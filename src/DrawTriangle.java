553470b8050b844d64862fc9bb71a1cae26a8b51
Edyta89 <styrczulaedyta89@gmail.com>
2022-03-24 14:43:27 +0100

    triangle

---
+++ src/DrawTriangle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as the number was
+
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        for (int i =1;i<=number;i++) {
+            for(int y=1; y<=i;y++)
+                System.out.print("*");
+            System.out.println();
+
+    }}
+
+    }
+
