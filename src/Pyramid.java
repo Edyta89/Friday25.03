d56b280b72db3efd0e8ab48421b701aac2ea64db
Edyta89 <styrczulaedyta89@gmail.com>
2022-03-24 15:28:59 +0100

    pyramid

---
+++ src/Pyramid.java
@@ -0,0 +1,20 @@
+public class Pyramid {
+    public static void main(String[] args) {
+        int lineCount = 4;
+
+   for (int i =0; i <=4;i++){
+       for (int j = 0; j<=4-i; j++){
+           System.out.print(" ");}
+       for (int k =0; k<=i;k++){
+           System.out.print(" *");
+       }
+       System.out.println();
+       }
+
+   }
+
+
+    }
+
+
+
