c0a68a0e411be1be4768f4a0c53f1a7b3d279e69
Edyta89 <styrczulaedyta89@gmail.com>
2022-03-24 09:16:58 +0100

    commit

--- src/MileToKmConverter.java
+++ src/MileToKmConverter.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MileToKmConverter {
+import java.util.Scanner;
+
+public class MileToKmConverter {
+    public static void main(String[] args) {
+
+        // Write a program that asks for a double that is a distance in miles,
+        // then it converts that value to kilometers and prints it
+        System.out.println("Request about distance in miles in decimal number");
+        Scanner scan = new Scanner(System.in);
+       double miles = scan.nextDouble();
+        double convert = 1.609344d ;
+        double km = miles* convert;
+       System.out.println("This is  "+ km +" km");
+
+
+    }
 }
