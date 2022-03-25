635faf522c30202c1951482276adffd8499f809b
Edyta89 <styrczulaedyta89@gmail.com>
2022-03-25 12:36:46 +0100

    changeelement

---
+++ src/Changeelement.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Changeelement {
+    public static void main(String[] args) {
+        // - Create an array variable named `numbers`
+//   with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the value of the fourth element (8) to 4
+// - Print the fourth element
+   int[] numbers = {1, 2, 3, 8, 5, 6};
+   numbers[3]=4;
+        System.out.println(Arrays.toString(numbers));
+   
+
+    }
+
+
+}
